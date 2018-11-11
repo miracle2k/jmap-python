6cf3b38640d18bb88ba44a25a5e67728a9b8879e
Michael Elsdörfer <michael@elsdoerfer.info>
2018-11-11 15:12:12 +0000

    Play around with a fallback module.

--- jmap/protocol/fallback.py
+++ jmap/protocol/fallback.py
@@ -2,3 +2,21 @@
 Implement all the "old" methods (as are in use by jmap-client), and redirect
 to tne new implementations?
 """
+
+from jmap.protocol.core import JmapBaseModule
+from jmap.protocol.models import MailboxGetArgs
+
+
+class FallbackModule(JmapBaseModule):
+
+    def __init__(self, email_module):
+        super().__init__()
+
+        self.email_module = email_module
+
+        self.methods = {
+            'getMailboxes': self.handle_get_mailboxes,
+        }    
+
+    def handle_get_mailboxes(self, context, args: MailboxGetArgs):
+        return self.email_module.handle_mailbox_get(context, args)
